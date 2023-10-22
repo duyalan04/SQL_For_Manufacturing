e009a1d56952ae4b8e552aa7e13d72db4cb8dfd7
duyalan04 <129828270+duyalan04@users.noreply.github.com>
2023-10-22 13:54:01 +0700

    Update Query.sql

--- Query.sql
+++ Query.sql
@@ -30,3 +30,79 @@
 --1 TRIG DELETE
 
 --2 TRANSACTION
+
+
+
+
+
+
+
+--PROC CỦA DUY
+--PROCEDURE
+--hien thi thong tin hoa don 
+CREATE PROC HienthithongtinHoadon
+AS
+BEGIN
+SELECT *from [Production.BillOfMaterials]
+WHERE PerAssemblyQty = 4 AND BOMLevel = 2
+END
+
+EXEC HienthithongtinHoadon
+
+--tao them 1 dong vao bang scrapReason voi cot ModifiedDate lay mac dinh la nam hien tai
+CREATE PROC Taothembang(
+@ScrapReasonID SMALLINT,
+@Name NVARCHAR(50),
+@ModifiedDate DATETIME)
+AS
+BEGIN 
+ IF @ModifiedDate IS NULL OR @ModifiedDate = ''
+SET @ModifiedDate = GETDATE();
+INSERT INTO [dbo].[Production.ScrapReason](ScrapReasonID,[Name],ModifiedDate)
+VALUES (@ScrapReasonID,@Name,@ModifiedDate)
+END
+SET IDENTITY_INSERT [dbo].[Production.ScrapReason] ON
+EXEC Taothembang 17,'Drill size medium',null
+select * from [dbo].[Production.ScrapReason]
+
+--CHƯA LÀM OUTPUT
+
+--xay dung thu tuc nhap vao --SỬA DÙM CÁI NÀY 
+--ALTER PROC p_ThongtinProduct(@ProductID INT)
+--AS 
+--BEGIN 
+--IF @ProductID NOT IN (SELECT pp.ProductID FROM [Production.Product] pp
+--INNER JOIN [Production.TransactionHistoryArchive] ptha 
+--ON pp.ProductID = ptha.ProductID
+--INNER JOIN [Production.TransactionHistory] pth
+--ON pth.[ReferenceOrderLineID] = ptha.[ReferenceOrderLineID])
+--PRINT(N'Mã Không tồn tại')
+--ELSE
+	--SELECT [Name],ProductNumber
+	--FROM [Production.Product] pp
+	--WHERE pp.ProductID = @ProductID
+--END
+
+CREATE PROC p_ThongtinProduct (@ProductID SMALLINT)
+AS
+BEGIN 
+	IF @ProductID NOT IN (SELECT ProductID FROM [Production.Product])
+	PRINT(N'Mã không tồn tại')
+ELSE
+	SELECT ProductID,[Name],ProductNumber
+	FROM [Production.Product]
+	WHERE ProductID = @ProductID
+END
+EXEC p_ThongtinProduct 531
+
+--thong tin historyarchive từ productid
+CREATE PROC p_ThongtinTHA (@ProductID INT)
+AS
+BEGIN
+	IF @ProductID NOT IN (SELECT ProductID FROM	[Production.TransactionHistoryArchive])
+	PRINT (N'Mã không tồn tại')
+ELSE
+	SELECT *FROM [Production.TransactionHistoryArchive]
+	WHERE ProductID = @ProductID
+END
+EXEC p_ThongtinTHA 716

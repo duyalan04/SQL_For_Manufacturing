61b7b2f9100623abbebed148c7bf77bdb8a3ca5e
HuuDuy <129828270+duyalan04@users.noreply.github.com>
2023-10-25 23:04:45 +0700

    Update FUNCTION Query.sql

--- Query.sql
+++ Query.sql
@@ -31,3 +31,27 @@ hello
 --1 TRIG DELETE
 
 --2 TRANSACTION
+
+
+
+--FUNCTION
+  
+--tổng số sản phẩm có thể bán được từ product 
+CREATE FUNCTION tong_nameproduct()
+RETURNS INT
+BEGIN
+RETURN (SELECT COUNT(ProductID) AS SOLUONGSANPHAM
+FROM [Production.Product]
+WHERE FinishedGoodsFlag=1)
+END
+
+  
+--Tổng giá tiền của các sản phẩm bán được
+CREATE FUNCTION sumofsaleproduct()
+RETURNS INT
+BEGIN
+RETURN (SELECT sum(ActualCost) AS TONGTIEN
+FROM [dbo].[Production.TransactionHistory]
+WHERE TransactionType='S')
+END
+SELECT dbo.sumofsaleproduct() AS TONGTIEN--
